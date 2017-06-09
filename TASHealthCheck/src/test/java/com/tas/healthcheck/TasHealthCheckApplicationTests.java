a3961ea4fbe9f2fde34da08d50f188ab69bb50d1
Gonzalez <luciano.j.gonzalez@accenturefederal.com>
2017-06-09 14:07:55 -0400

    Project template

---
+++ TASHealthCheck/src/test/java/com/tas/healthcheck/TasHealthCheckApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tas.healthcheck;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TasHealthCheckApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
